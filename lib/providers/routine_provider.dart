dba972cf269702b2463a93462eff53aaf1e807f3
Onesto58 <lukino.mi@gmail.com>
2026-04-26 11:40:28 +0200

    up

--- lib/providers/routine_provider.dart
+++ lib/providers/routine_provider.dart
@@ -4,7 +4,7 @@ import '../models/routine.dart';
 
 final routineServiceProvider = Provider((ref) => RoutineFirestoreService());
 
-final routinesStreamProvider = StreamProvider<List<Routine>>((ref) {
+final routinesStreamProvider = StreamProvider.keepAlive<List<Routine>>((ref) {
   final service = ref.watch(routineServiceProvider);
   return service.getRoutines();
 });
